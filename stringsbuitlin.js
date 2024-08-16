4af9f7988ec5510d53b39c28f88b68beb7c78a12
Akshita Srivastava <akshita.srivastava.1505@gmail.com>
2024-08-17 00:15:58 +0530

    functionsjs

--- stringsbuitlin.js
+++ stringsbuitlin.js
@@ -1,5 +1,6 @@
 //there's no change in original string 
 let s="  akshita  ";
-console.log(s.toUpperCase());
-console.log(s);
-console.log(s.trim());
+// console.log(s.toUpperCase());
+// console.log(s);
+// console.log(s.trim());
+console.log(s.indexOf("a"));
