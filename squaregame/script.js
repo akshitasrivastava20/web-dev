7f75f64ddb42706ad681cb8f3de4495b42dde701
Akshita Srivastava <akshita.srivastava.1505@gmail.com>
2024-08-20 23:44:40 +0530

    eventlistener

---
+++ squaregame/script.js
@@ -0,0 +1,11 @@
+let x= document.getElementById("one");
+let y= document.getElementById("two");
+let z= document.getElementById("three");
+let q= document.getElementById("four");
+
+x.addEventListener("mouseenter",function(){
+    s1.innerHTML="<h1>5</h1>"
+})
+x.addEventListener("mouseleave",function(){
+    s1.innerHTML="<h1>1</h1>"
+})
