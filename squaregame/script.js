28d530f9c5bd562c09b219a7f73eeef53f910a95
Akshita Srivastava <akshita.srivastava.1505@gmail.com>
2024-08-22 00:07:28 +0530

    .

--- squaregame/script.js
+++ squaregame/script.js
@@ -1,11 +1,35 @@
 let x= document.getElementById("one");
+
+x.addEventListener("mouseenter",function(){
+  let r= Math.floor(Math.random()*100);
+  x.innerHTML=`<h1>${r}</h1>`;
+})
+
+x.addEventListener("mouseleave",function(){
+  x.innerHTML="<h1>1</h1>";
+})
+
 let y= document.getElementById("two");
+clr="red";
+y.addEventListener("mouseenter",function(){
+  if(clr=="red"){
+    y.style.backgroundColor="red";
+    clr="green";
+  }
+  else if(clr=="green"){
+    y.style.backgroundColor="green";
+    clr="blue";
+  }
+  else{
+    y.style.backgroundColor="blue";
+    clr="red";
+
+  }
+})
+y.addEventListener("mouseleave",function(){
+  y.style.backgroundColor="white";
+})
+
 let z= document.getElementById("three");
 let q= document.getElementById("four");
 
-x.addEventListener("mouseenter",function(){
-    s1.innerHTML="<h1>5</h1>"
-})
-x.addEventListener("mouseleave",function(){
-    s1.innerHTML="<h1>1</h1>"
-})
